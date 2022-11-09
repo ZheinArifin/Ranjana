206b82c5b517a48ad04900d7a958d147f0a86d7c
Arifin <a.zaenularifin25@gmail.com>
2022-11-09 12:28:03 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,447 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from preview.colorlib.com/theme/dusk/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 06 Nov 2022 17:33:59 GMT -->
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.gstatic.com/">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&amp;family=Open+Sans:wght@400;700&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href="A.fonts%2c%2c_icomoon%2c%2c_style.css%2bfonts%2c%2c_flaticon%2c%2c_font%2c%2c_flaticon.css%2bcss%2c%2c_bootstrap.css%2bcss%2c%2c_tiny-slider.css%2bcss%2c%2c_glightbox.min.css%2bcss%2c%2c_aos.css%2bcss%2c%2c_style.css%2cMcc.15Nd4FnzDQ.css" />
+<title>Dusk &mdash; Free Bootstrap 5 Website Template by Colorlib</title>
+<script nonce="eee79983-b41e-47ef-a1df-8543fc300f73">(function(w,d){!function(Z,_,ba,bb){Z.zarazData=Z.zarazData||{};Z.zarazData.executed=[];Z.zaraz={deferred:[],listeners:[]};Z.zaraz.q=[];Z.zaraz._f=function(bc){return function(){var bd=Array.prototype.slice.call(arguments);Z.zaraz.q.push({m:bc,a:bd})}};for(const be of["track","set","debug"])Z.zaraz[be]=Z.zaraz._f(be);Z.zaraz.init=()=>{var bf=_.getElementsByTagName(bb)[0],bg=_.createElement(bb),bh=_.getElementsByTagName("title")[0];bh&&(Z.zarazData.t=_.getElementsByTagName("title")[0].text);Z.zarazData.x=Math.random();Z.zarazData.w=Z.screen.width;Z.zarazData.h=Z.screen.height;Z.zarazData.j=Z.innerHeight;Z.zarazData.e=Z.innerWidth;Z.zarazData.l=Z.location.href;Z.zarazData.r=_.referrer;Z.zarazData.k=Z.screen.colorDepth;Z.zarazData.n=_.characterSet;Z.zarazData.o=(new Date).getTimezoneOffset();Z.zarazData.q=[];for(;Z.zaraz.q.length;){const bl=Z.zaraz.q.shift();Z.zarazData.q.push(bl)}bg.defer=!0;for(const bm of[localStorage,sessionStorage])Object.keys(bm||{}).filter((bo=>bo.startsWith("_zaraz_"))).forEach((bn=>{try{Z.zarazData["z_"+bn.slice(7)]=JSON.parse(bm.getItem(bn))}catch{Z.zarazData["z_"+bn.slice(7)]=bm.getItem(bn)}}));bg.referrerPolicy="origin";bg.src="../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(Z.zarazData)));bf.parentNode.insertBefore(bg,bf)};["complete","interactive"].includes(_.readyState)?zaraz.init():Z.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
+<body>
+<div class="site-mobile-menu site-navbar-target">
+<div class="site-mobile-menu-header">
+<div class="site-mobile-menu-close">
+<span class="icofont-close js-menu-toggle"></span>
+</div>
+</div>
+<div class="site-mobile-menu-body"></div>
+</div>
+<nav class="site-nav">
+<div class="container">
+<div class="site-navigation">
+<a href="index.html" class="logo m-0 float-left">Dusk</a>
+<ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right">
+<li class="active"><a href="index.html">Home</a></li>
+<li class="has-children">
+<a href="#">Dropdown</a>
+<ul class="dropdown">
+<li><a href="#">Menu One</a></li>
+<li class="has-children">
+<a href="#">Menu Two</a>
+<ul class="dropdown">
+<li><a href="#">Sub Menu One</a></li>
+<li><a href="#">Sub Menu Two</a></li>
+<li><a href="#">Sub Menu Three</a></li>
+</ul>
+</li>
+<li><a href="#">Menu Three</a></li>
+</ul>
+</li>
+<li><a href="services.html">Services</a></li>
+<li><a href="about.html">About</a></li>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="contact.html">Contact</a></li>
+</ul>
+<a href="#" class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light" data-toggle="collapse" data-target="#main-navbar">
+<span></span>
+</a>
+</div>
+</div>
+</nav>
+<div class="hero overlay" style="background-image: url('images/hero_1.jpg')">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-7 text-center mx-auto">
+<h1 class="heading text-white" data-aos="fade-up" data-aos-delay="0">We Deliver Your Package On Time</h1>
+<h3 class="subheaidng text-white mb-5 h5" data-aos="fade-up" data-aos-delay="100">Far far away, behind the word mountains</h3>
+<p data-aos="fade-up" data-aos-delay="200">
+<a href="#" class="btn btn-primary text-white mr-2">Partner with us</a>
+<a href="https://www.youtube.com/watch?v=mwtbEGNABWU" class="btn btn-white glightbox"><span class="icon-play mr-2"></span>Watch</a>
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="section sec-1 bg-curve">
+<div class="container">
+<div class="row">
+<div class="col-lg-7 mx-auto">
+<div class="d-flex box align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+<div class="img text-center">
+<div>
+<img src="images/svg/delivery-box.svg" alt="Image" class="img-fluid">
+</div>
+</div>
+<div class="text">
+<h2>Are you a Medium to Larged Size Business?</h2>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<div class="d-flex justify-content-between">
+<a href="#" class="more">Get started</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="section sec-2">
+<div class="container">
+<div class="row justify-content-between align-items-center">
+<div class="col-lg-7 mb-5" data-aos="fade-up" data-aos-delay="0">
+<div class="img-wrap has-bg">
+<img src="images/about_1.jpg" alt="Image" class="img-fluid rounded">
+</div>
+</div>
+<div class="col-lg-4 pl-md-5" data-aos="fade-up" data-aos-delay="100">
+<h2 class="heading">Proof On Display</h2>
+<div class="line mb-4"></div>
+<p class="lead mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<div class="row list-icons">
+<div class="col-4 col-lg-4">
+<span class="flaticon-delivery-box"></span>
+<span class="d-block">24/7 Fast Delivery</span>
+</div>
+<div class="col-4 col-lg-4">
+<span class="flaticon-shopping-online"></span>
+<span class="d-block">90% Repeat Purchase</span>
+</div>
+<div class="col-4 col-lg-4">
+<span class="flaticon-delivery"></span>
+<span class="d-block">We Deliver Worldwide</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="section">
+<div class="container">
+<div class="row heading-wrap">
+<div class="col-lg-7 mx-auto text-center" data-aos="fade-up" data-aos-delay="0">
+<h2 class="heading">Choose Your Ideal Pack</h2>
+<div class="line mx-auto"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="100">
+<div class="pricing-block">
+<strong class="price">$12</strong>
+<h3 class="font-weight-bold">Basic</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<ul class="pricing-content-list mb-4">
+<li>Far far away, behind the word</li>
+<li>far from the countries Vokalia and Consonantia</li>
+<li class="disabled">there live the blind texts.</li>
+<li class="disabled">there live the blind texts.</li>
+<li class="disabled">there live the blind texts.</li>
+</ul>
+<a href="#">Get started</a>
+</div>
+</div>
+<div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="200">
+<div class="pricing-block">
+<strong class="price">$49</strong>
+<h3 class="font-weight-bold">Standard</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<ul class="pricing-content-list mb-4">
+<li>Far far away, behind the word</li>
+<li>far from the countries Vokalia and Consonantia</li>
+<li>there live the blind texts.</li>
+<li class="disabled">there live the blind texts.</li>
+<li class="disabled">there live the blind texts.</li>
+</ul>
+<a href="#">Get started</a>
+</div>
+</div>
+<div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="300">
+<div class="pricing-block">
+<strong class="price">$79</strong>
+<h3 class="font-weight-bold">Premium</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<ul class="pricing-content-list mb-4">
+<li>Far far away, behind the word</li>
+<li>far from the countries Vokalia and Consonantia</li>
+<li>there live the blind texts.</li>
+<li>there live the blind texts.</li>
+<li class="disabled">there live the blind texts.</li>
+</ul>
+<a href="#">Get started</a>
+</div>
+</div>
+<div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="400">
+<div class="pricing-block">
+<strong class="price">$199</strong>
+<h3 class="font-weight-bold">Advanced</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<ul class="pricing-content-list mb-4">
+<li>Far far away, behind the word</li>
+<li>far from the countries Vokalia and Consonantia</li>
+<li>there live the blind texts.</li>
+<li>there live the blind texts.</li>
+<li>there live the blind texts.</li>
+</ul>
+<a href="#">Get started</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="section">
+<div class="container">
+<div class="row heading-wrap">
+<div class="col-lg-7 mx-auto text-center" data-aos="fade-up" data-aos-delay="0">
+<h2 class="heading">Services</h2>
+<div class="line mx-auto"></div>
+</div>
+</div>
+<div class="row list-icons list-icons-large">
+<div class="col-sm-6 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="100">
+<span class="flaticon-delivery-box"></span>
+<h3 class="font-weight-bold">24/7 Fast Delivery</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</div>
+<div class="col-sm-6 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="200">
+<span class="flaticon-shopping-online"></span>
+<h3 class="font-weight-bold">90% Repeat Purchase</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</div>
+<div class="col-sm-6 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="300">
+<span class="flaticon-delivery"></span>
+<h3 class="font-weight-bold">We Deliver Worldwide</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</div>
+<div class="col-sm-6 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="400">
+<span class="flaticon-weighing-scale"></span>
+<h3 class="font-weight-bold">Scale Sufficiently</h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="section bg-light">
+<div class="container">
+<div class="row mb-5">
+<div class="col-lg-7" data-aos="fade-up" data-aos-delay="0">
+<h2 class="heading">What people says about us</h2>
+<div class="line me-auto"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-12" data-aos="fade-up" data-aos-delay="100">
+<div class="testimonial-slider-wrap">
+<div id="testimonial-nav">
+<span class="prev" data-controls="prev"><span class="icon-chevron-left"></span></span>
+<span class="next" data-controls="next"><span class="icon-chevron-right"></span></span>
+</div>
+<div class="testimonial-slider">
+<div class="item">
+<div class="d-block d-lg-flex testimonial align-items-stretch">
+<div class="img">
+<img src="images/person_1.jpg" alt="Image" class="img-fluid">
+</div>
+<div class="text align-self-center">
+<span class="quote">&ldquo;</span>
+<blockquote>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</blockquote>
+<strong class="d-block">Maria Golpe</strong>
+<span class="text-black-50">Managing Director</span>
+</div>
+</div>
+</div>
+<div class="item">
+<div class="d-block d-lg-flex testimonial align-items-stretch">
+<div class="img">
+<img src="images/person_2.jpg" alt="Image" class="img-fluid">
+</div>
+<div class="text align-self-center">
+<span class="quote">&ldquo;</span>
+<blockquote>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</blockquote>
+<strong class="d-block">Maria Golpe</strong>
+<span class="text-black-50">Managing Director</span>
+</div>
+</div>
+</div>
+<div class="item">
+<div class="d-block d-lg-flex testimonial align-items-stretch">
+<div class="img">
+<img src="images/person_3.jpg" alt="Image" class="img-fluid">
+</div>
+<div class="text align-self-center">
+<span class="quote">&ldquo;</span>
+<blockquote>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</blockquote>
+<strong class="d-block">Maria Golpe</strong>
+<span class="text-black-50">Managing Director</span>
+</div>
+</div>
+</div>
+<div class="item">
+<div class="d-block d-lg-flex testimonial align-items-stretch">
+<div class="img">
+<img src="images/person_4.jpg" alt="Image" class="img-fluid">
+</div>
+<div class="text align-self-center">
+<span class="quote">&ldquo;</span>
+<blockquote>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</blockquote>
+<strong class="d-block">Maria Golpe</strong>
+<span class="text-black-50">Managing Director</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="section blog-entries-section">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-lg-7 section-title text-center mb-5" data-aos="fade-up" data-aos-delay="0">
+<h2 class="heading">Latest Blog Posts</h2>
+<div class="line mx-auto"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-6 col-sm-6 col-md-4 mb-4 mb-lg-0 col-lg" data-aos="fade-up" data-aos-delay="0">
+<div class="post-entry">
+<span class="date">May 12th, 2020</span>
+<h3><a href="#">Far far away, behind the word mountains</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+</div>
+</div>
+<div class="col-6 col-sm-6 col-md-4 mb-4 mb-lg-0 col-lg" data-aos="fade-up" data-aos-delay="100">
+<div class="post-entry">
+<span class="date">May 12th, 2020</span>
+<h3><a href="#">Far far away, behind the word mountains</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+</div>
+</div>
+<div class="col-6 col-sm-6 col-md-4 mb-4 mb-lg-0 col-lg" data-aos="fade-up" data-aos-delay="200">
+<div class="post-entry">
+<span class="date">May 12th, 2020</span>
+<h3><a href="#">Far far away, behind the word mountains</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+</div>
+</div>
+<div class="col-6 col-sm-6 col-md-4 mb-4 mb-lg-0 col-lg" data-aos="fade-up" data-aos-delay="300">
+<div class="post-entry">
+<span class="date">May 12th, 2020</span>
+<h3><a href="#">Far far away, behind the word mountains</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+</div>
+</div>
+<div class="col-6 col-sm-6 col-md-4 mb-4 mb-lg-0 col-lg" data-aos="fade-up" data-aos-delay="400">
+<div class="post-entry">
+<span class="date">May 12th, 2020</span>
+<h3><a href="#">Far far away, behind the word mountains</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="site-footer">
+<div class="container">
+<div class="row">
+<div class="col-lg-4">
+<div class="widget">
+<h3>About Foundation<span class="text-primary">.</span> </h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+</div> 
+<div class="widget">
+<h3>Connect</h3>
+<ul class="list-unstyled social">
+<li><a href="#"><span class="icon-instagram"></span></a></li>
+<li><a href="#"><span class="icon-twitter"></span></a></li>
+<li><a href="#"><span class="icon-facebook"></span></a></li>
+<li><a href="#"><span class="icon-linkedin"></span></a></li>
+<li><a href="#"><span class="icon-pinterest"></span></a></li>
+<li><a href="#"><span class="icon-dribbble"></span></a></li>
+</ul>
+</div> 
+</div> 
+<div class="col-lg-2 ml-auto">
+<div class="widget">
+<h3>Links</h3>
+<ul class="list-unstyled float-left links">
+<li><a href="#">About us</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">News</a></li>
+<li><a href="#">Careers</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</div> 
+</div> 
+<div class="col-lg-2">
+<div class="widget">
+<h3>Company</h3>
+<ul class="list-unstyled float-left links">
+<li><a href="#">About us</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">News</a></li>
+<li><a href="#">Careers</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+</div> 
+</div> 
+<div class="col-lg-3">
+<div class="widget">
+<h3>Contact</h3>
+<address>43 Raymouth Rd. Baltemoer, London 3910</address>
+<ul class="list-unstyled links mb-4">
+<li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+<li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+<li><a href="https://preview.colorlib.com/cdn-cgi/l/email-protection#9bf2f5fdf4dbf6e2fff4f6faf2f5b5f8f4f6"><span class="__cf_email__" data-cfemail="e58c8b838aa5889c818a88848c8bcb868a88">[email&#160;protected]</span></a></li>
+</ul>
+</div> 
+</div> 
+</div> 
+<div class="row mt-5">
+<div class="col-12 text-center">
+<p>
+
+Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+</p>
+</div>
+</div>
+</div> 
+</div> 
+
+<div id="overlayer"></div>
+<div class="loader">
+<div class="spinner-border text-primary" role="status">
+<span class="visually-hidden">Loading...</span>
+</div>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/tiny-slider.js"></script>
+<script src="js/aos.js%2bnavbar.js%2bcounter.js%2bglightbox.min.js%2bcustom.js.pagespeed.jc.TDZe19B6iY.js"></script><script>eval(mod_pagespeed_sCNJx8yW2n);</script>
+<script>eval(mod_pagespeed_sKs3e5FcNz);</script>
+<script>eval(mod_pagespeed_vVpcJZ7q74);</script>
+<script>eval(mod_pagespeed_CH9Z3_1R5i);</script>
+<script>eval(mod_pagespeed_P5IXatWlwP);</script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-23581568-13');
+		</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"765f93c7cce1a3e9","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.11.0","si":100}' crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/dusk/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 06 Nov 2022 17:34:05 GMT -->
+</html>
